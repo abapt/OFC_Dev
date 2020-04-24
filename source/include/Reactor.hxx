ad6a7221286c0d3f597099244484e39685fba982
Nicolas Thiolliere <nicolas.thiolliere@subatech.in2p3.fr>
2020-04-24 17:48:37 +0200

    Add status and some debug...

--- source/include/Reactor.hxx
+++ source/include/Reactor.hxx
@@ -7,41 +7,42 @@ class Reactor
 {
 public :
 	Reactor(); //Constructeur simple
-	Reactor(double fStartingTime, double fLifeTime, double fPower, double fMassHN,double fBurnUp, 
-			double fLoadFactor); //Constructeur
+	Reactor(double fStartingTime, double fLifeTime, double fPower, double fMassHN, double fBurnUp,
+	        double fLoadFactor); //Constructeur
 	~Reactor(); //Destructeur
 
 	double GetMassHN() const {
-    return fMassHN;
- 	}  // Masse de noyaux lourds au début du cycle
- 	double GetBurnUp() const {
-    return fBurnUp;
- 	}  // BurnUp à la fin du cycle
- 	double GetLoadFactor() const {
-    return fLoadFactor;
- 	}  // facteur de "remplissage" du réacteur
- 	double GetPower() const {
-    return fPower;
-  	}  // temps de cycle du réacteur
-  	double GetEnrichissement() const {
-    return fEnrichissement;
-  	}
-  	double GetCrossSection() const {
-    return fCrossSection;
-  	}
-  	void SetName(string Name){
-  		fName = Name;
-  	}
-  	void SetMassHN(double Mass) {
-    	fMassHN = Mass;
-  	}  // défini le masse de noyaux fissiles au début du cycle
+		return fMassHN;
+	}  // Masse de noyaux lourds au début du cycle
+	double GetBurnUp() const {
+		return fBurnUp;
+	}  // BurnUp à la fin du cycle
+	double GetLoadFactor() const {
+		return fLoadFactor;
+	}  // facteur de "remplissage" du réacteur
+	double GetPower() const {
+		return fPower;
+	}  // temps de cycle du réacteur
+	double GetEnrichissement() const {
+		return fEnrichissement;
+	}
+	double GetCrossSection() const {
+		return fCrossSection;
+	}
+	void SetName(string Name) {
+		fName = Name;
+	}
+	void SetMassHN(double Mass) {
+		fMassHN = Mass;
+	}  // défini le masse de noyaux fissiles au début du cycle
 
-  	void Evolution(double t);  // Evolution jusqu'à t 			-- A voir
- 	// void Dump();   Vider   							-- A voir 
-  	// void SetNewFuel();   Remplace le combustible 		-- A voir dans scénario
+	void Evolution(double t);  // Evolution jusqu'à t 			-- A voir
+	// void Dump();   Vider   							-- A voir
+	// void SetNewFuel();   Remplace le combustible 		-- A voir dans scénario
 
 	void CalculateU5Enrichment(double fBurnUp);
 
+	void BuildStatusVector();
 private :
 
 	string fName;
@@ -52,8 +53,17 @@ private :
 	double fCycleTime;
 	double fStartingTime;
 	double fLifeTime;
-	const double fEnrichissement = 0.04;
-	const double fCrossSection = 46.933e-24;
+	double fEnrichissement;
+	double fCrossSection;
+
+	// Status of the reactor
+	// 0 : Nothing
+	// 1 : Fresh Fuel Loading
+	// 2 : Spent Fuel Pushing
+	// 4 : Evolution
+	vector <int> fstatus;
+
+	//Evolution data
 	vector<double> fFlux;
 	vector<double> fMassU5Evolution;					//-- A voir
 	vector<double> fMassU8Evolution;					//-- A voir
--- source/src/Reactor.cxx
+++ source/src/Reactor.cxx
@@ -7,19 +7,14 @@
 ////////////////////////////////////////////////////////////////
 Reactor::Reactor() {
   (*this).SetName("Reactor");
-  
-  fMassHN = 100;
-  fPower = 3e9;
-  fLoadFactor = 0.75;
-  fBurnUp = 35; 
-  fCycleTime = (fBurnUp * 1e9 / (fPower)*fMassHN * 24);
-  fStartingTime = 5*365.25*24;
-  fLifeTime = 50*365.25*24; 
-
 }
 
-Reactor::Reactor(double StartingTime, double LifeTime, double power, double MassHN,double BurnUp, 
-				double LoadFactor) {
+Reactor::Reactor(double StartingTime,
+                 double LifeTime,
+                 double power,
+                 double MassHN,
+                 double BurnUp,
+                 double LoadFactor) {
   (*this).SetName("Reactor");
 
   fLoadFactor = LoadFactor;
@@ -28,7 +23,9 @@ Reactor::Reactor(double StartingTime, double LifeTime, double power, double Mass
   fBurnUp = BurnUp;
   fLifeTime = LifeTime;
 
-  fCycleTime = (fBurnUp * 1e9 / fPower)*(fMassHN * 24);
+  fCycleTime = (fBurnUp * 1e9 / fPower) * (fMassHN * 24);
+
+  BuildStatusVector();
 }
 
 ////////////////////////////////////////////////////////////////
@@ -40,37 +37,41 @@ Reactor::~Reactor() {}
 ///////// Fonctions ////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
 void Reactor::CalculateU5Enrichment(double fBurnUp) {
-	fMassHN = 0.0135139 + 0.000563569*fBurnUp + 1.34642e-06*fBurnUp*fBurnUp; // fBurnUp en GWj/t
+  fMassHN = 0.0135139 + 0.000563569 * fBurnUp + 1.34642e-06 * fBurnUp * fBurnUp; // fBurnUp en GWj/t
 }
 
 void Reactor::Evolution(double fCycleTime) {
-/*
-FIT PWR - 3.26% en U5
-p0                        =   2.4807e+14   +/-   1.17036e+12
-p1                        =   4.6258e+13   +/-   1.36441e+12
-p2                        = -1.68784e+12   +/-   3.28195e+11
+  /*
+  FIT PWR - 3.26% en U5
+  p0                        =   2.4807e+14   +/-   1.17036e+12
+  p1                        =   4.6258e+13   +/-   1.36441e+12
+  p2                        = -1.68784e+12   +/-   3.28195e+11
 
-FIT PWR - 4.20% en U5
-p0                        =  2.28774e+14   +/-   1.43852e+12
-p1                        =  3.81756e+13   +/-   1.92004e+12
-p2                        = -8.50596e+11   +/-   5.27476e+11
-*/
+  FIT PWR - 4.20% en U5
+  p0                        =  2.28774e+14   +/-   1.43852e+12
+  p1                        =  3.81756e+13   +/-   1.92004e+12
+  p2                        = -8.50596e+11   +/-   5.27476e+11
+  */
 
-/*
-  int i(0);
-  do
-  {
-    fFlux[i]= 4.10569e15 - 8.75127e10*i + 5.31149e6*i*i - 142.44206*i*i*i + 0.00204*i*i*i*i - 1.55273e-8*i*i*i*i*i + 4.9082e-14*i*i*i*i*i*i;
-    if (i%fCycleTime==0)
+  /*
+    int i(0);
+    do
     {
-      fMassU5Evolution[i] = fMassHN ;
-      fMassU8Evolution[i] = fMassHN/fEnrichissement;
-    }
-    else
-    {
-      fMassU5Evolution[i] = fMassU5Evolution[i-1]*exp(-fFlux[i]*fCrossSection*(365*24*60*60));
-    }
-    i++;
-  } while (i<fLifeTime);
-*/
+      fFlux[i]= 4.10569e15 - 8.75127e10*i + 5.31149e6*i*i - 142.44206*i*i*i + 0.00204*i*i*i*i - 1.55273e-8*i*i*i*i*i + 4.9082e-14*i*i*i*i*i*i;
+      if (i%fCycleTime==0)
+      {
+        fMassU5Evolution[i] = fMassHN ;
+        fMassU8Evolution[i] = fMassHN/fEnrichissement;
+      }
+      else
+      {
+        fMassU5Evolution[i] = fMassU5Evolution[i-1]*exp(-fFlux[i]*fCrossSection*(365*24*60*60));
+      }
+      i++;
+    } while (i<fLifeTime);
+  */
+}
+
+void Reactor::BuildStatusVector(double fCycleTime) {
+
 }
