fe6a5721af8639ddaa28dbaaba6cfe1500017156
Nicolas Thiolliere <nicolas.thiolliere@subatech.in2p3.fr>
2020-04-24 17:27:14 +0200

    Debug...

--- source/src/Reactor.cxx
+++ source/src/Reactor.cxx
@@ -6,7 +6,9 @@
 ///////// Constructeur /////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
 Reactor::Reactor() {
-  MassHN = 100;
+  (*this).SetName("Reactor");
+  
+  fMassHN = 100;
   fPower = 3e9;
   fLoadFactor = 0.75;
   fBurnUp = 35; 
@@ -42,9 +44,23 @@ void Reactor::CalculateU5Enrichment(double fBurnUp) {
 }
 
 void Reactor::Evolution(double fCycleTime) {
+/*
+FIT PWR - 3.26% en U5
+p0                        =   2.4807e+14   +/-   1.17036e+12
+p1                        =   4.6258e+13   +/-   1.36441e+12
+p2                        = -1.68784e+12   +/-   3.28195e+11
+
+FIT PWR - 4.20% en U5
+p0                        =  2.28774e+14   +/-   1.43852e+12
+p1                        =  3.81756e+13   +/-   1.92004e+12
+p2                        = -8.50596e+11   +/-   5.27476e+11
+*/
+
+/*
+  int i(0);
   do
   {
-    fFlux[i]= 4.10569e15 - 8.75127e10*i + 5.31149e6*i² - 142.44206*i³ + 0.00204*i⁴ - 1.55273e-8*i⁵ + 4.9082e-14*i⁶;
+    fFlux[i]= 4.10569e15 - 8.75127e10*i + 5.31149e6*i*i - 142.44206*i*i*i + 0.00204*i*i*i*i - 1.55273e-8*i*i*i*i*i + 4.9082e-14*i*i*i*i*i*i;
     if (i%fCycleTime==0)
     {
       fMassU5Evolution[i] = fMassHN ;
@@ -53,8 +69,8 @@ void Reactor::Evolution(double fCycleTime) {
     else
     {
       fMassU5Evolution[i] = fMassU5Evolution[i-1]*exp(-fFlux[i]*fCrossSection*(365*24*60*60));
-      /*fMassU8Evolution[i] = fMassHN*; */
     }
     i++;
   } while (i<fLifeTime);
+*/
 }
